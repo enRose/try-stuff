71a6ea91508fd7a55830cd029e8bf7cab1ccafba
Yini <34562396+enRose@users.noreply.github.com>
2021-09-11 00:27:44 +1200

    expression bodied members

---
+++ c-sharp-monkey/APIMonkey/CSharp6.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace APIMonkeyCSharp6
+{
+    public class ExpressionBodiedMethods
+    {
+        string _firstName = "Yini";
+        string _lastName = "Yin";
+        
+        public void DoStuff() => Console.WriteLine($"{_firstName} {_lastName}");
+    }
+
+    public class ExpressionBodiedReadOnlyProps
+    {
+        string FirstName => "Yini";
+        string LastName => "Yin";
+    }
+}
--- c-sharp-monkey/APIMonkey/CSharp7.cs
+++ c-sharp-monkey/APIMonkey/CSharp7.cs
@@ -1,13 +1,23 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
-namespace APIMonkey
+namespace APIMonkeyCSharp7
 {
-    public class CSharp7
+    public class ExpressionBodiedProps
     {
-        public CSharp7()
-        {
-        }
+        private string _name;
 
+        public ExpressionBodiedProps(string n) => Name = n;
+
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+    }
+
+    public class Discards
+    {
         public void DoStuff()
         {
             _ = FireForget();
@@ -18,6 +28,25 @@ namespace APIMonkey
             await Task.Delay(whatIsYourName == "Monkey King" ? 50 : 300);
 
             return true;
+        }
+    }
+
+    public class InlineVarDeclaration
+    {
+        public void DoStuff()
+        {
+            // inlined_variable_declaration = true
+            if (int.TryParse("1", out int x))
+            {
+                Console.WriteLine(x);
+            }
+
+            // inlined_variable_declaration = false
+            int y;
+            if (int.TryParse("1", out y))
+            {
+                Console.WriteLine(y);
+            }
         }
     }
 }
